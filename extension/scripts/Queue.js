07d5a7c77cfa3fff9cc41fa078f5c88cd5fc148f
ndineth100 <ndineth100@gmail.com>
2019-04-25 15:49:14 +0530

    Fug fix 60

--- extension/scripts/Queue.js
+++ extension/scripts/Queue.js
@@ -1,20 +1,6 @@
-const redis = require('redis');
+const {client, llenAsync, lpopAsync, sismemberAsync, saddAsync, rpushAsync} = require('./redis-client')
 var Job = require('./Job')
-const {promisify} = require('util');
-
-
-// Create Redis Client
-let client = redis.createClient();
-
-client.on('connect', function(){
-    console.log('Package Queue Initiated a Connection to Redis...');
-});
-
-const llenAsync = promisify(client.llen).bind(client);
-const lpopAsync = promisify(client.lpop).bind(client);
-const sismemberAsync = promisify(client.sismember).bind(client);
-const saddAsync = promisify(client.sadd).bind(client);
-const rpushAsync = promisify(client.rpush).bind(client);
+const {promisify} = require('util')
 
 var Queue = function () {
 
